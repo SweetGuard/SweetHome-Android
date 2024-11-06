642dc14def172ebe8f12fbdee32e0cbe6c0bed65
yxnxx <jh4rgh0@gmail.com>
2024-11-06 15:54:51 +0900

    Feat: add logo

--- app/src/main/java/com/example/sweethome/ui/MainScreen.kt
+++ app/src/main/java/com/example/sweethome/ui/MainScreen.kt
@@ -22,13 +22,13 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.R
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.compose.ui.window.DialogProperties
+import com.example.sweethome.R
 import com.example.sweethome.ui.theme.SweetHomeTheme
 
 @Composable
@@ -42,16 +42,19 @@ fun MainScreen(
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-//        Image(
-//            painter = painterResource(id = R.drawable.ic_logo),
-//            contentDescription = "Logo",
-//            modifier = Modifier.height(80.dp))
-        Spacer(modifier = Modifier.height(16.dp))
-        Text(
-            text = "Sweet Home",
-            fontSize = 24.sp,
-            color = Color.Black
-        )
+        Image(
+            painter = painterResource(id = R.drawable.logo),
+            contentDescription = "Logo",
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(10.dp)
+                .height(100.dp))
+//        Text(
+//            text = "Sweet Home",
+//            fontSize = 30.sp,
+//            color = Color.Black
+//        )
+        Spacer(modifier = Modifier.height(20.dp))
         Button(
             onClick = onNavigateToRecording,
             modifier = Modifier
